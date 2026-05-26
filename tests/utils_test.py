cef9b84d2de7975a6385c443638dccec4736ff97
alexcubed2006 <alexyakubitski@gmail.com>
2026-05-26 16:25:01 -0400

    deleted tests, added ansible configs for CD

--- tests/utils_test.py
+++
@@ -1,8 +0,0 @@
-from src.utils import normalize_day
-
-def test_normalize_day():
-    assert normalize_day("пн") == "Понедельник"
-    assert normalize_day("вторник") == "Вторник"
-    assert normalize_day("Ср") == "Среда"
-    assert normalize_day("Воскресенье") == "Воскресенье"
-    assert normalize_day("неизвестный") == "Неизвестный"
